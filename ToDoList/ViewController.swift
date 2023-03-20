6bcbfcb08ff0bb585ae80009d21d91ba382f1b0a
Steven YAMBOS <stevenyambos@Mini-de-Steven.lan>
2023-03-20 23:44:34 +0100

    Gestion du rafraîchissement

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -9,8 +9,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
  
-    
-    
+    // Composant UI qui permet d'afficher des éléments en liste
     private let table: UITableView = {
         let table = UITableView()
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -37,19 +36,28 @@ class ViewController: UIViewController, UITableViewDataSource {
         // Confirmer
         alert.addAction(UIAlertAction(
             title: "Confirmer",
-            style: .default, handler: {(_) in}))
+            style: .default, handler: { [weak self] (_) in
+                // si le champs de texte vient d'être créée...
+                if let field = alert.textFields?.first {
+                    // ...et si le texte est remplie
+                    if let text = field.text, !text.isEmpty {
+                        DispatchQueue.main.async {
+                            var currentItem = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                            currentItem.append(text)
+                            UserDefaults.standard.setValue(currentItem, forKey: "items")
+                            self?.items.append(text)
+                            self?.table.reloadData()
+                        }
+                    }
+                }
+                
+            }))
         present(alert, animated: true)
-        // si le champs de texte vient d'être créée...
-        if let field = alert.textFields?.first {
-            // ...et si le texte est remplie
-            if let text = field.text, !text.isEmpty {
-                print(text)
-            }
-        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
         title = "To Do List"
         view.addSubview(table)
         table.dataSource = self
@@ -62,13 +70,17 @@ class ViewController: UIViewController, UITableViewDataSource {
         table.frame = view.bounds
     }
     
+    // Les TableView dans iOS affichent des rangées de contenu à défilement vertical dans une seule colonne.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        // le nombre de lignes correspondra aux nombre d'éléments créés
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
         return cell
+        
     }
 
 
